f28f89a6fba7d2b36c3b4444504359866ebdecff
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-06-08 12:54:08 +0300

    Project Initialization

---
+++ src/test/java/ru/tsu/hits/internshipsystem/InternshipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.internshipsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
