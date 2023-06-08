ed62421d43bb4734bdc8629dbb5844ebd9688857
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-06-08 13:02:30 +0300

    Project Initialization

---
+++ Company-Service/src/main/java/ru/tsu/hits/companyservice/CompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.companyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyServiceApplication.class, args);
+    }
+
+}
---
+++ Internship-Application/src/test/java/ru/tsu/hits/internshipapplication/InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.internshipapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
