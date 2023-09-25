67e7ad9ed2c8e457fe6e299ba8d253c53ca04f21
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-09-26 03:11:28 +0700

    Small refactoring in the User-Service - Removed useless annotations

--- User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtRequest.java
+++ User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtRequest.java
@@ -1,11 +1,7 @@
 package ru.tsu.hits.userservice.dto;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class JwtRequest {
     private String email;
     private String password;
--- User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtResponse.java
+++ User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtResponse.java
@@ -1,10 +1,8 @@
 package ru.tsu.hits.userservice.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class JwtResponse {
     private String jwtToken;
 }
