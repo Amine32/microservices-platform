9871b563463292ec72bf6a6bdf094a9732e7591f
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-09-26 14:39:18 +0700

    Fixed Jwt Constructors

--- User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtRequest.java
+++ User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtRequest.java
@@ -1,7 +1,9 @@
 package ru.tsu.hits.userservice.dto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class JwtRequest {
     private String email;
     private String password;
--- User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtResponse.java
+++ User-Service/src/main/java/ru/tsu/hits/userservice/dto/JwtResponse.java
@@ -1,8 +1,11 @@
 package ru.tsu.hits.userservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class JwtResponse {
     private String jwtToken;
 }
