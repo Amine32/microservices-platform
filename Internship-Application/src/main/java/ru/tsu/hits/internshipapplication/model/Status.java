7f396445a74baaeef9147d0f5de8e820e4cd2105
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-10-02 02:11:08 +0700

    Added additional statuses in Status.java

--- Internship-Application/src/main/java/ru/tsu/hits/internshipapplication/model/Status.java
+++ Internship-Application/src/main/java/ru/tsu/hits/internshipapplication/model/Status.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public enum Status {
     NEW("Подана заявка"),
-    REJECTED("Отказано"),
+    REJECTED("В оффере отказано"),
     INTERVIEW_IS_APPOINTED("Назначено собеседование"),
     INTERVIEW_PASSED("Пройдено собеседование"),
+    WAITING_FOR_COMPANY_ANSWER("Ожидается ответ от компании после прохождения собеседования"),
+    WAITING_FOR_STUDENT_ANSWER("Ожидается ответ от студента"),
     OFFER_ISSUED("Предложен оффер"),
     OFFER_ACCEPTED("Принят оффер"),
     OFFER_REJECTED("Не принят оффер");
