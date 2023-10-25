4d4d4e6e9057bc0c1e4c10ddaa7b21737182c56e
Amine Amatoury <30076185+amatouryamine@users.noreply.github.com>
2023-10-26 06:06:55 +0700

    Added Curator role and microservice initialized.

---
+++ Curator-Service/src/main/java/ru/tsu/hits/curatorservice/CuratorServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.curatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuratorServiceApplication.class, args);
+	}
+
+}
---
+++ Curator-Service/src/test/java/ru/tsu/hits/curatorservice/CuratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.curatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
